c4ae367f5c5374e8ff7bb895b4a61e486c199db8
xterikatx <caluerika@gmail.com>
2023-08-28 01:41:18 -0300

    chore: add register screen

---
+++ src/screens/login/index.js
@@ -0,0 +1,50 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import backgroundImage from "../../../assets/background-signin.jpeg";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <KeyboardAwareScrollView className="bg-black rounded-t-3xl">
+      <View>
+        <Image source={backgroundImage} className="object-cover h-80 w-full" />
+      </View>
+      <View className="bg-white h-screen rounded-t-3xl">
+        <Text className="text-[#222] text-3xl font-semibold text-center py-3 mt-3">
+          Sign in
+        </Text>
+        <View className="items-center">
+          <TextInput
+            className="tracking-widest bg-gray-100 rounded-lg w-80 text-base py-2 px-1 mx-3 mb-5"
+            placeholder="Email"
+            value={email}
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            className="tracking-widest bg-gray-100 rounded-lg w-80 text-base py-2 px-1 mx-3 mb-5"
+            placeholder="Password"
+            value={password}
+            textContentType="password"
+            autoCorrect={false}
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <TouchableOpacity className="py-3 bg-[#222] rounded items-center my-5 mx-20">
+          <Text className="text-lg text-white">Login</Text>
+        </TouchableOpacity>
+        <View className="flex-row space-x-2 justify-end mr-10 mt-5">
+          <Text>Don't have an account?</Text>
+          <TouchableOpacity>
+            <Text className="underline">Sign up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
+  );
+}
